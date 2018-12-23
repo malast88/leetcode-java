1a3484ccd7294dfc5a16de9d5edf77bbd8e6d39a
malast <malast88@gmail.com>
2018-12-23 17:10:48 +0100

    Added 'Two sum' solution

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # leetcode-java
+
+![](https://img.shields.io/travis/malast88/leetcode-java.svg)
+![](https://img.shields.io/codecov/c/github/malast88/leetcode-java.svg)
+
+My solutions of [Leetcode](https://leetcode.com) problems.
---
+++ src/test/java/malast88/leetcode/problems/twosum/TwoSumTest.java
@@ -0,0 +1,48 @@
+package malast88.leetcode.problems.twosum;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+@ExtendWith(MockitoExtension.class)
+public class TwoSumTest {
+
+    @InjectMocks
+    private TwoSum sut;
+
+    private static Stream<Arguments> getTwoSumArgs() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTwoSumArgs")
+    public void itShouldReturnTwoSum(int[] nums, int target, int[] expectedResult) {
+        // init
+
+        // test
+        int[] result = sut.twoSum(nums, target);
+
+        // verify
+        assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void itShouldThrowIllegalArgumentExceptionWhenWrongInputProvided() {
+        // init
+
+        // test & verify
+        assertThrows(IllegalArgumentException.class, () -> sut.twoSum(new int[]{}, 0));
+    }
+}
