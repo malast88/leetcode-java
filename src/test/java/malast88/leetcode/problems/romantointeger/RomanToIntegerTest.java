8e87b8f82027145dd1dedc46bf712ed80df1685d
malast <malast88@gmail.com>
2018-12-24 18:59:51 +0100

    Added 'Roman to integer' solution

---
+++ src/test/java/malast88/leetcode/problems/romantointeger/RomanToIntegerTest.java
@@ -0,0 +1,43 @@
+package malast88.leetcode.problems.romantointeger;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class RomanToIntegerTest {
+
+    @InjectMocks
+    private RomanToInteger sut;
+
+    private static Stream<Arguments> getReverseIntegerArgs() {
+        return Stream.of(
+                Arguments.of("III", 3),
+                Arguments.of("IV", 4),
+                Arguments.of("IX", 9),
+                Arguments.of("LVIII", 58),
+                Arguments.of("MCMXCIV", 1994),
+                Arguments.of("CDI", 401),
+                Arguments.of("CDI_", 401)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getReverseIntegerArgs")
+    public void itShouldReverseInteger(String s, int expectedResult) {
+        // init
+
+        // test
+        int result = sut.romanToInt(s);
+
+        // verify
+        assertEquals(expectedResult, result);
+    }
+}
