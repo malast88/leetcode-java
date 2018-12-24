7d0fc32671c24a9b2333a3929073154db2467e24
malast <malast88@gmail.com>
2018-12-24 18:13:01 +0100

    Added 'Palindrome number' solution

---
+++ src/test/java/malast88/leetcode/problems/palindromenumber/PalindromeNumberTest.java
@@ -0,0 +1,41 @@
+package malast88.leetcode.problems.palindromenumber;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class PalindromeNumberTest {
+
+    @InjectMocks
+    private PalindromeNumber sut;
+
+    private static Stream<Arguments> getReverseIntegerArgs() {
+        return Stream.of(
+                Arguments.of(123, false),
+                Arguments.of(0, true),
+                Arguments.of(121, true),
+                Arguments.of(12321, true),
+                Arguments.of(-12321, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getReverseIntegerArgs")
+    public void itShouldReverseInteger(int x, boolean expectedResult) {
+        // init
+
+        // test
+        boolean result = sut.isPalindrome(x);
+
+        // verify
+        assertEquals(expectedResult, result);
+    }
+}
