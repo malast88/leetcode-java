b7b10e437383b93cc376e3d63dc15607f50e12d4
malast <malast88@gmail.com>
2019-01-10 23:03:23 +0100

    Added solution for 'Best time to buy and sell stock'

---
+++ src/main/java/malast88/leetcode/problems/besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,26 @@
+package malast88.leetcode.problems.besttimetobuyandsellstock;
+
+public class BestTimeToBuyAndSellStock {
+
+    /**
+     * Solution for https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+     * Note that you cannot sell a stock before you buy one.
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int max = -1;
+        int maxProfit = 0;
+        for (int i=prices.length-1;i>=0;i--) {
+            if (prices[i] > max) {
+                max = prices[i];
+            }
+            if (max - prices[i] > maxProfit) {
+                maxProfit = max - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
